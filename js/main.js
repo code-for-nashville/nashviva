5147866b4752d87270bb961e79289e3eaf82bdc4
JamesNix <jamesfnix@gmail.com>
2014-11-08 12:50:42 -0600

    Map appears with mapquest data

--- js/main.js
+++ js/main.js
@@ -8,6 +8,13 @@
 
   }
 
-  var map = L.map('map').setView([36.21547, -86.72707], 13);
+  var map = L.map('map').setView([36.165818, -86.784245], 13);
+
+  L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+    subdomains: ['otile1','otile2','otile3','otile4']
+}).addTo( map );
+
+  var marker = L.marker([51.5, -0.09]).addTo(map);
 
 })();
