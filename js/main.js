89bfc2a34fd8b82d1307c090d7a34da351b7ae40
Max Shenfield <shenfieldmax@gmail.com>
2017-05-10 09:52:22 -0500

    Got tilemap up and running

--- js/main.js
+++ js/main.js
@@ -32,9 +32,8 @@
       communityIcon = new L.Icon({iconUrl:'../images/home-color.png',iconSize:[45,45]});
 
   //create the layer for the map from MapQuest
-  L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-    subdomains: ['otile1','otile2','otile3','otile4']
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
 }).addTo( map );
 
 //Convert our JSON files into latitude longitude points in the "Points"
