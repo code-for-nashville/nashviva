cb38665e3f14a386237b855ba6880b365b14c307
Max Shenfield <shenfieldmax@gmail.com>
2017-05-10 09:16:39 -0500

    Added community centers and fixed JSON links
    Closes #54 and closes #41. The JSON datasets were being linked to "/dataset.json" instead of "/datasets/dataset.json".  Community centers data was there, but it hadn't been added to the app.

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@
 
   }
 
-  var datasetButtons= ['#fire-station','#police-station','#wifi', '#parks'];
+  var datasetButtons= ['#fire-station','#police-station','#wifi', '#parks','#community'];
 
   datasetButtons.forEach(function(dataset){
     $(dataset).click(function(){
@@ -20,15 +20,16 @@
   var map = L.map('map').setView([36.165818, -86.784245], 13);
   //varables for the map. points are latitude and longitude points
   //markers are leaflet markers
-  var firePoints=[], parksPoints=[], policePoints=[], wifiPoints=[],
-    fireMarkers=[], policeMarkers=[], wifiMarkers=[], parksMarkers=[];
+  var firePoints=[], parksPoints=[], policePoints=[], wifiPoints=[], communityPoints=[],
+    fireMarkers=[], policeMarkers=[], wifiMarkers=[], parksMarkers=[], communityMarkers=[]
   //set up the icons for the markers
   var fireIcon = new L.Icon({iconUrl:'../images/firestation-color.png',
                             iconSize:[45,45]}),
       policeIcon = new L.Icon({iconUrl:'../images/policestation-color.png',
                               iconSize:[45,45]}),
       wifiIcon = new L.Icon({iconUrl:'../images/wifi-color.png',iconSize:[45,45]}),
-      parksIcon = new L.Icon({iconUrl:'../images/park-color.png',iconSize:[45,45]});
+      parksIcon = new L.Icon({iconUrl:'../images/park-color.png',iconSize:[45,45]}),
+      communityIcon = new L.Icon({iconUrl:'../images/home-color.png',iconSize:[45,45]});
 
   //create the layer for the map from MapQuest
   L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
@@ -38,22 +39,27 @@
 
 //Convert our JSON files into latitude longitude points in the "Points"
 //variables
-$.get('/firestation-cleaned.json',function(data){
+$.get('/datasets/community-centers-cleaned.json',function(data){
+  data.forEach(function(loc){
+    communityPoints.push([+loc.location[1],+loc.location[0]]);
+  });
+});
+$.get('/datasets/firestation-cleaned.json',function(data){
   data.forEach(function(loc){
     firePoints.push([+loc.location[1],+loc.location[0]]);
   });
 });
-$.get('/police-cleaned.json',function(data){
+$.get('/datasets/police-cleaned.json',function(data){
   data.forEach(function(loc){
     policePoints.push([+loc.location[1],+loc.location[0]]);
   });
 });
-$.get('/hotspot-cleaned.json', function(data){
+$.get('/datasets/hotspot-cleaned.json', function(data){
   data.forEach(function(loc){
     wifiPoints.push([+loc.location[1],+loc.location[0]]);
   });
 });
-$.get('/parks-cleaned.json',function(data){
+$.get('/datasets/parks-cleaned.json',function(data){
   data.forEach(function(loc){
     parksPoints.push([+loc.location[0],+loc.location[1],loc.name]);
   });
@@ -76,9 +82,8 @@ function toggleIcon(type){
     case 'parks':
       setMarkersFor(parksPoints,parksMarkers,parksIcon);
       break;
-    case 'recycling':
-      // setMarkersFor(recyclingPoints, recyclingMarkers, recyclingIcon);
-      break;
+    case 'community':
+      setMarkersFor(communityPoints,communityMarkers,communityIcon);
   }
   function setMarkersFor(points, markers, icon){
     //set markers makes the array of leaflet markers if
