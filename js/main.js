0d9ab15532a17dd9227a40d0bf69cd1682a311d1
Max Shenfield <shenfieldmax@gmail.com>
2017-05-17 23:40:09 -0500

    Remove semicolons

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 (function() {
-  'use strict';
-  $(document).ready(initialize);
+  'use strict'
+  $(document).ready(initialize)
 
   function initialize() {}
 
-  var datasetButtons = ['#fire-station', '#police-station', '#wifi', '#parks', '#community'];
+  var datasetButtons = ['#fire-station', '#police-station', '#wifi', '#parks', '#community']
 
   datasetButtons.forEach(function(dataset){
-    $(dataset).prop('disabled', true);
+    $(dataset).prop('disabled', true)
     $(dataset).click(function(){
       //add toggle for showing icons on map
-      toggleIcon($(this).attr('id'));
-    });
-  });
+      toggleIcon($(this).attr('id'))
+    })
+  })
 
   /*
   The boundaries for Nashville are
@@ -26,7 +26,7 @@
     // center and zoom
     [36.165818, -86.784245],
     12
-  );
+  )
 
   //varables for the map. points are latitude and longitude points
   //markers are leaflet markers
@@ -39,7 +39,7 @@
       policeMarkers = [],
       wifiMarkers = [],
       parksMarkers = [],
-      communityMarkers = [];
+      communityMarkers = []
   //set up the icons for the markers
   var fireIcon = new L.Icon({
         iconUrl: '../images/firestation-color.png',
@@ -60,63 +60,63 @@
       communityIcon = new L.Icon({
         iconUrl: '../images/home-color.png',
         iconSize: [45, 45]
-      });
+      })
 
   //create the layer for the map from MapQuest
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
+    attribution: '&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map)
 
   function processJSON(data, locationKey) {
     return data.data.map(function(el) {
-      var lat = el[locationKey][1];
-      var long = el[locationKey][2];
-      var location = ['', ''];
+      var lat = el[locationKey][1]
+      var long = el[locationKey][2]
+      var location = ['', '']
       if (lat !== null && long !== null) {
-        return [long, lat];
+        return [long, lat]
       }
-      return location;
-    });
+      return location
+    })
   }
 
   $.get('/datasets/community-centers-cleaned.json', function(data) {
     data.forEach(function(loc) {
-      communityPoints.push([loc.location[1], loc.location[0]]);
-    });
-    $('#community').prop('disabled', false);
-  });
+      communityPoints.push([loc.location[1], loc.location[0]])
+    })
+    $('#community').prop('disabled', false)
+  })
 
   $.get('https://data.nashville.gov/api/views/frq9-a5iv/rows.json', function(data) {
-    var fireLocs = processJSON(data, 13);
+    var fireLocs = processJSON(data, 13)
     fireLocs.forEach(function(loc) {
-      firePoints.push([loc[1], loc[0]]);
-    });
-    $('#fire-station').prop('disabled', false);
-  });
+      firePoints.push([loc[1], loc[0]])
+    })
+    $('#fire-station').prop('disabled', false)
+  })
 
   $.get('https://data.nashville.gov/api/views/y5ik-ut5s/rows.json', function(data) {
-    var policeLocs = processJSON(data, 17);
+    var policeLocs = processJSON(data, 17)
     policeLocs.forEach(function(loc) {
-      policePoints.push([loc[1], loc[0]]);
-    });
-    $('#police-station').prop('disabled', false);
-  });
+      policePoints.push([loc[1], loc[0]])
+    })
+    $('#police-station').prop('disabled', false)
+  })
 
   $.get('https://data.nashville.gov/api/views/4ugp-s85t/rows.json', function(data) {
-    var wifiLocs = processJSON(data, 11);
+    var wifiLocs = processJSON(data, 11)
     wifiLocs.forEach(function(loc) {
-      wifiPoints.push([loc[1], loc[0]]);
-    });
-    $('#wifi').prop('disabled', false);
-  });
+      wifiPoints.push([loc[1], loc[0]])
+    })
+    $('#wifi').prop('disabled', false)
+  })
 
   $.get('https://data.nashville.gov/api/views/74d7-b74t/rows.json', function(data) {
-    var parksLocs = processJSON(data, 42);
+    var parksLocs = processJSON(data, 42)
     parksLocs.forEach(function(loc) {
-      parksPoints.push([loc[1], loc[0]]);
-    });
-    $('#parks').prop('disabled', false);
-  });
+      parksPoints.push([loc[1], loc[0]])
+    })
+    $('#parks').prop('disabled', false)
+  })
 
   function toggleIcon(type) {
     //toggleIcon takes the type of markers we want to toggle
@@ -124,19 +124,19 @@
     //whether they are showing or not
     switch (type) {
       case 'fire-station':
-        setMarkersFor(firePoints, fireMarkers, fireIcon);
-        break;
+        setMarkersFor(firePoints, fireMarkers, fireIcon)
+        break
       case 'police-station':
-        setMarkersFor(policePoints, policeMarkers, policeIcon);
-        break;
+        setMarkersFor(policePoints, policeMarkers, policeIcon)
+        break
       case 'wifi':
-        setMarkersFor(wifiPoints, wifiMarkers, wifiIcon);
-        break;
+        setMarkersFor(wifiPoints, wifiMarkers, wifiIcon)
+        break
       case 'parks':
-        setMarkersFor(parksPoints, parksMarkers, parksIcon);
-        break;
+        setMarkersFor(parksPoints, parksMarkers, parksIcon)
+        break
       case 'community':
-        setMarkersFor(communityPoints, communityMarkers, communityIcon);
+        setMarkersFor(communityPoints, communityMarkers, communityIcon)
     }
     function setMarkersFor(points, markers, icon) {
       //set markers makes the array of leaflet markers if
@@ -145,25 +145,25 @@
         points.forEach(function(el) {
           //for each point, make a new marker and push it into
           //the markers array
-          var mark = L.marker(el.slice(0, 2), { icon: icon });
+          var mark = L.marker(el.slice(0, 2), { icon: icon })
           if (el[2]) {
-            mark.bindPopup('<p>' + el[2] + '</p>');
+            mark.bindPopup('<p>' + el[2] + '</p>')
           }
-          markers.push(mark.addTo(map));
-        });
+          markers.push(mark.addTo(map))
+        })
       } else {
         //if they already exist, then toggle the opacity to show or
         //hide the markers
         markers.forEach(function(el) {
-          if (el.options.opacity === 0) el.setOpacity(1);
-          else el.setOpacity(0);
-          el.closePopup();
-        });
+          if (el.options.opacity === 0) el.setOpacity(1)
+          else el.setOpacity(0)
+          el.closePopup()
+        })
       }
     }
   }
-})();
+})()
 
 $(window).resize(function() {
-  $('#map').width($('.app-description').width() - 250);
-});
+  $('#map').width($('.app-description').width() - 250)
+})
